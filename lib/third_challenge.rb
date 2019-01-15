14d3ea7241f43bf7e9718ca12105de8708ce206e
Kian Edalat <akedalat@gmail.com>
2019-01-15 17:43:59 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,32 +2,33 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague"
+        :name => "Lord Montague",
         :age => "53"
         },
       :matriarch => {
-        :name => "Lady Montagu"
+        :name => "Lady Montagu",
         :age => "54"
         },
       :hero => {
-        :name => "Romeo"
-        :age => "15"
+        :name => "Romeo",
+        :age => "15",
         :status => "alive"
         },
       :hero_friends => []
    },
    :capulet => {
       :patriarch => {
-        :name => "Lord capulet"
+        :name => "Lord capulet",
         :age => "50"
         },
       :matriarch => {
-        :name => "Lady capulet"
+        :name => "Lady capulet",
         :age => "51"
         },
       :heroine => {
-        :name => "Juliet"
-        :age => "15"
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }
